01b337b07fd157a8048233dc9771ba28c648764a
webAnkit23 <ankitsingh2323@gmail.com>
2024-05-22 10:59:31 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import UserProvider from './Context/userProvider.jsx'
+import UserProvider from './Context/UserProvider.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
      <UserProvider >
          <App />
